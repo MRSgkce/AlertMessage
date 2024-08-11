bb84ddc308deb50da26e83ea68a83b88fcc963b0
MRSgkce <mursidegokce985@gmail.com>
2024-08-11 22:14:33 +0300

    email kontrolü yapılırken uyarı mesajı

--- AlertMessage/ViewController.swift
+++ AlertMessage/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  AlertMessage
-//
-//  Created by Mürşide Gökçe on 11.08.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sifre2: UITextField!
+    @IBOutlet weak var sifre: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpTiklandi(_ sender: Any) {
+        let uyariMesaji = UIAlertController(title: "Hata Mesajı", message: "Email yanlış verildi", preferredStyle: .alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: .default) { action in
+            // OK butonuna tıklanınca olacaklar
+            print("OK butonuna tıklandı")
+        }
+        
+        uyariMesaji.addAction(okButton)
+        self.present(uyariMesaji, animated: true, completion: nil)
+    }
 }
-
